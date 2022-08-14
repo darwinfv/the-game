7cd73bbfe14a8efd0176b76dcdf7c46a3e9dc8db
Darwin Vaz <dvaz@purdue.edu>
2022-08-14 15:21:42 -0500

    add functionality for stack shadow when selecting deck

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,5 +1,4 @@
 export function click(event, data) {
-  console.log(event, data);
   let parent = getParent(event.target);
   parent.style.zIndex = 1;
 }
@@ -11,7 +10,29 @@ export function drop(event, data) {
 
 export function track(e, data) {
   // setPosition({ x: data.x, y: data.y });
-  console.log(e.pageX, e.pageY);
+  // console.log(e.pageX, e.pageY);
+
+  let card = checkHover(e.pageX, e.pageY)
+  if (card) {
+    flicker(card);
+  }
+}
+
+function flicker(element) {
+  element.style.backgroundColor = 'grey';
+}
+
+function checkHover(x, y) {
+  let cards = document.getElementsByClassName('card');
+  for (let i = 0; i < 4; i++) {
+    let rect = cards[i].getBoundingClientRect();
+    if (x > rect.left && x < rect.right && y > rect.top && y < rect.bottom) {
+      return cards[i];
+    } else {
+      cards[i].style.backgroundColor = 'white';
+    }
+  }
+  return false;
 }
 
 function getParent(element) {
