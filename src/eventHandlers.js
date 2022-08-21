d1936a3d379bf75ccf03f7a2b396a9990659c32a
Darwin Vaz <dvaz@purdue.edu>
2022-08-20 23:55:52 -0500

    add validation before playing a card

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -4,20 +4,19 @@ export function click(event, data) {
 }
 
 export function drop(event, data) {
-  let card = checkHover(event.pageX, event.pageY)
+  let parent = getParent(event.target);
+  let card = checkHover(event.pageX, event.pageY, value(parent));
   if (card) {
-    let parent = getParent(event.target)
-    changeNumber(card, parent.childNodes[0].childNodes[0].childNodes[0].innerHTML);
+    changeNumber(card, value(parent));
     card.style.backgroundColor = 'white';
     return true;
   } else {
-      let parent = getParent(event.target);
-      parent.style.zIndex = 0;
+    parent.style.zIndex = 0;
   }
 }
 
 export function track(event, data) {
-  let card = checkHover(event.pageX, event.pageY)
+  let card = checkHover(event.pageX, event.pageY, value(getParent(event.target)))
   if (card) {
     flicker(card);
   }
@@ -35,17 +34,33 @@ function changeNumber(element, number) {
   element.childNodes[2].childNodes[1].innerHTML = number;
 }
 
-function checkHover(x, y) {
+function checkHover(x, y, number) {
   let cards = document.getElementsByClassName('card');
   for (let i = 0; i < 4; i++) {
     let rect = cards[i].getBoundingClientRect();
     if (x > rect.left && x < rect.right && y > rect.top && y < rect.bottom) {
-      return cards[i];
+      if (checkValid(value(getParent(cards[i])), number, i)) {
+        return cards[i];
+      }
+      return;
     } else {
       cards[i].style.backgroundColor = 'white';
     }
   }
-  return false;
+}
+
+function checkValid(dest, source, dir) {
+  source = parseInt(source);
+  dest = parseInt(dest);
+  if (dir < 2) {
+    return source > dest || source === dest - 10;
+  } else {
+    return source < dest || source === dest + 10;
+  }
+}
+
+function value(element) {
+  return element.childNodes[element.childNodes.length - 1].childNodes[0].childNodes[0].innerHTML;
 }
 
 function getParent(element) {
